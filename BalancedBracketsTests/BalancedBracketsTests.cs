2030752ae90ce0fa51624b85bf4335934a94c4c3
Tasha L <natasha.lewis.lalor@gmail.com>
2022-08-31 19:40:16 -0500

    Finished for Tasha and Soniya

--- BalancedBracketsTests/BalancedBracketsTests.cs
+++ BalancedBracketsTests/BalancedBracketsTests.cs
@@ -13,5 +13,86 @@ namespace BalancedBracketsTests
         {
             Assert.AreEqual(true, true);
         }
+
+        [TestMethod]
+        public void EmptyStringReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets(""));
+        }
+
+
+        [TestMethod]
+        public void OnlyBracketsReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[]"));
+        }
+
+        [TestMethod]
+        public void SimpleBracketsAndOneWordReturnsTrue()
+        {
+            //arrange
+            string tester = "[Potato]";
+            //action and assert
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets(tester));
+        }
+
+        [TestMethod]
+        public void EmptyTwoBracketsReturnsTrue()
+        {
+            string tester = "[[]]";
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets(tester));
+        }
+
+        [TestMethod]
+        public void SimpleTwoBracketsAroundStringReturnsTrue()
+        {
+            string tester = "[[Friends]]";
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets(tester));
+        }
+
+        [TestMethod]
+        public void TwoOpenBracketsOneCloserReturnsFalse()
+        {
+            string tester = "[[Friends]";
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets(tester));
+        }
+
+        [TestMethod]
+        public void BracketsInReverseReturnFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("]["));
+        }
+
+        [TestMethod]
+        public void MultipleBracketsInReverseReturnFalse()
+        {
+            string tester = "][Booleans][AnotherGoodOne][";
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets(tester));
+        }
+
+        [TestMethod]
+        public void OneOpenBracketReturnsFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("["));
+        }
+
+        [TestMethod]
+        public void OneCloseBracketReturnsFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("]"));
+        }
+
+        [TestMethod]
+        public void BalancedBracketsBeforeStringReturnTrue()
+        {
+            string tester = "[[]]Friends";
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets(tester));
+        }
+        [TestMethod]
+        public void BalancedBracketsAfterStringReturnTrue()
+        {
+            string tester = "Friends[[]][]";
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets(tester));
+        }
     }
 }
